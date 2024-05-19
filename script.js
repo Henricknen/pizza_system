7278e5d2bb3549643ceb3437c0493020ba5df922
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-19 18:47:49 -0300

    adicionando_'nome'_'valor'_e_'imagens'_das_pizzas_na_tela

--- script.js
+++ script.js
@@ -9,5 +9,10 @@ const cs = (el)=> {     // 'cs' retorna um array com itens encontrados
 pizzaJson.map((item, index)=> {     // mapeando 'pizzaJson'
     let pizzaItem = c('.models .pizza-item'). cloneNode(true);       // função 'cloneNode' clona elemento de classe 'pizza-item' do html
 
+    pizzaItem.querySelector('.pizza-item--img img'). src = item.img;     // seleçionando elemento de classe 'pizza-item--img' do html, 'src' inseri o caminho da imagem da pizza
+    pizzaItem.querySelector('.pizza-item--price'). innerHTML = `R$ ${item.price.toFixed(2)}`;       // 'toFixed' fixa dois algarismos depois da virgula
+    pizzaItem.querySelector('.pizza-item--name'). innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc'). innerHTML = item.description;
+
     c('.pizza-area'). append(pizzaItem);       // 'append' adiçionando o elemento 'pizzaItem'
 });

60ccb017b26d850415cfdf56817aaefa1768ac71
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-20 16:39:03 -0300

    inserindo_'imagem'_'tamanho'_e_'preco'

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let modalQt = 1;        // variável 
 const c = (el)=> {      // constante 'c' reçebe um elemento 'el' e retorna 'document.querySelector', será ultilizada para evitar extensão excessiva de código
     return document.querySelector(el);
 }
@@ -17,10 +18,21 @@ pizzaJson.map((item, index)=> {     // mapeando 'pizzaJson'
     pizzaItem.querySelector('a'). addEventListener('click', (e)=> {        // seleçionando tag 'a' e adiçionando um 'evento' de click
         e.preventDefault();     // 'preventDefault' vai previnir a ação padrão de 'click' da tag 'a'
         let key = e.target.closest('.pizza-item'). getAttribute('data-key');
+        modalQt = 1;        // seta a quandida como 1
 
-        // c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaBig img').src = pizzaJson[key].img;       // preenchendo o 'modal' com informações da pizza
         c('.pizzaInfo h1'). innerHTML = pizzaJson[key]. name;
         c('.pizzaInfo--desc'). innerHTML = pizzaJson[key]. description;
+        c('.pizzaInfo--actualPrice'). innerHTML = `R$ ${pizzaJson[key]. price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected'). classList.remove('selected');       // removendo 'selected' para desseleçionar o item
+        cs('.pizzaInfo--size'). forEach((size, sizeIndex)=> {
+            if(sizeIndex == 2) {        // verificando qual 'sizeIndex' para adiçionar o 'selected'
+                size.classList.add('selected');
+            }
+            size.querySelector('span'). innerHTML = pizzaJson[key]. sizes[sizeIndex];
+        });
+
+        c('.pizzaInfo--qt'). innerHTML = modalQt;
 
         c('.pizzaWindowArea'). style.opacity = 0;
         c('.pizzaWindowArea'). style.display = 'flex';      // alterando 'display' do elemento de classe 'pizzaWindowArea' para torna-lo visivel na tela
